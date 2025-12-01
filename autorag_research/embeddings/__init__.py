719339d55523815876f0c2b66ec7b5923507eff8
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-12-01 12:48:36 +0900

    add Base classes for multi vector and multi vector multi modal embeddings (ColBERT, ColPali, etc..)

---
+++ autorag_research/embeddings/__init__.py
@@ -0,0 +1,11 @@
+from autorag_research.embeddings.base import (
+    MultiVectorBaseEmbedding,
+    MultiVectorEmbedding,
+    MultiVectorMultiModalEmbedding,
+)
+
+__all__ = [
+    "MultiVectorBaseEmbedding",
+    "MultiVectorEmbedding",
+    "MultiVectorMultiModalEmbedding",
+]
