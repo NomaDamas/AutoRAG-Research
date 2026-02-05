2be90876cd321d6f86badb989130ee5869ad850b
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2026-02-05 18:46:18 +0900

    run linter

--- autorag_research/orm/service/retrieval_pipeline.py
+++ autorag_research/orm/service/retrieval_pipeline.py
@@ -320,7 +320,6 @@ class RetrievalPipelineService(BaseService):
             )
             return [self._make_retrieval_result(chunk, score) for chunk, score in results]
 
-
     def bm25_search(
         self,
         query_ids: list[int | str],
